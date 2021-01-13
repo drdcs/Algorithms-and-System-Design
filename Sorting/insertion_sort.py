fedc50b54ab7e7bfcf2c3cb8316809103857ad8e
drdcs <diptihdl@gmail.com>
2021-01-13 23:41:46 +0530

    test stage

--- Sorting/insertion_sort.py
+++ Sorting/insertion_sort.py
@@ -2,7 +2,7 @@ def insertion_sort(sequence):
     length = len(sequence)
     for i in range(1, length):
         value_to_sort = sequence[i]
-        while sequence[i-1] > value_to_sort:
+        while sequence[i-1] > value_to_sort and i > 0:
             sequence[i] , sequence[i-1] = sequence[i-1], sequence[i]
             i -= 1
     return sequence
