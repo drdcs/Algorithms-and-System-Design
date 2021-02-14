71f7a24871307ee6f53c69d6e12fcd57e782ca0b
drdcs <diptihdl@gmail.com>
2021-02-14 22:18:53 +0530

    recursion

---
+++ Recursion/startRec.py
@@ -0,0 +1,16 @@
+def printNum(n: int):
+    if n==7:
+        return 1
+    print(n, end= "->")
+    printNum(n+1)
+
+def printNumReverse(n: int):
+    if n==0:
+        return 1
+    print(n, end="->")
+    printNumReverse(n-1)
+if __name__ == '__main__':
+    printNum(1)
+    print('\n')
+    printNumReverse(6)
+    print('\n')
